1ee4026123527832d0ea9146ba709b8204cf192b
Aleksi Knuutila <aleksi@knuutila.net>
2022-08-08 14:22:15 +0300

    Update data_configs.py

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -3,6 +3,13 @@ from configs.paths_config import dataset_paths
 
 
 DATASETS = {
+	'streetview': {
+		'transforms': transforms_config.EncodeTransforms,
+		'train_source_root': dataset_paths['train_data'],
+		'train_target_root': dataset_paths['train_data'],
+		'test_source_root': dataset_paths['test_data'],
+		'test_target_root': dataset_paths['test_data'],
+	},
 	'ffhq_hypernet': {
 		'transforms': transforms_config.EncodeTransforms,
 		'train_source_root': dataset_paths['ffhq'],
