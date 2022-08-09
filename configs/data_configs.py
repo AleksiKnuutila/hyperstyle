290cae27a7ccc11791fdf5527260de9b37611436
Aleksi Knuutila <aleksi@knuutila.net>
2022-08-09 13:17:28 +0300

    Update data_configs.py

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -10,4 +10,14 @@ DATASETS = {
 		'test_source_root': dataset_paths['test_data'],
 		'test_target_root': dataset_paths['test_data'],
 	},
+	'ffhq_hypernet_pre_extract': {
+		'transforms': transforms_config.NoFlipTransforms,
+		'train_source_root': dataset_paths['train_data'],
+		'train_target_root': dataset_paths['train_data'],
+		'train_latents_path': dataset_paths['train_latents'],
+		'test_source_root': dataset_paths['test_data'],
+		'test_target_root': dataset_paths['test_data'],
+		'test_latents_path': dataset_paths['test_latents']
+	},
+
 }
